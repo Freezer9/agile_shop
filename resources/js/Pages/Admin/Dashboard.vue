23a55a40059e6251c4c8c35c68d9a8fa14784dbb
alifnaufal1 <naufalmenjangan@gmail.com>
2024-05-06 09:33:10 +0700

    feat: chart dashboard admin

--- resources/js/Pages/Admin/Dashboard.vue
+++ resources/js/Pages/Admin/Dashboard.vue
@@ -19,7 +19,15 @@
             </div>
             <div
                 class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-96 mb-4"
-            ></div>
+            >
+                <div class="flex justify-center items-center h-full">
+                    <Bar
+                        id="my-chart-id"
+                        :options="chartOptions"
+                        :data="chartData"
+                    />
+                </div>
+            </div>
             <div class="grid grid-cols-2 gap-4 mb-4">
                 <div
                     class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72"
@@ -65,4 +73,65 @@ import AdminLayout from "./Components/AdminLayout.vue";
 onMounted(() => {
     initFlowbite();
 });
+</script>
+
+<script>
+import { Bar } from "vue-chartjs";
+import { onMounted } from "vue";
+import { initFlowbite } from "flowbite";
+import AdminLayout from "./Components/AdminLayout.vue";
+// initialize components based on data attribute selectors
+import {
+    Chart as ChartJS,
+    Title,
+    Tooltip,
+    Legend,
+    BarElement,
+    CategoryScale,
+    LinearScale,
+} from "chart.js";
+
+onMounted(() => {
+    initFlowbite();
+});
+
+ChartJS.register(
+    Title,
+    Tooltip,
+    Legend,
+    BarElement,
+    CategoryScale,
+    LinearScale
+);
+
+export default {
+    name: "SalesChart",
+    components: { Bar },
+    data() {
+        return {
+            chartData: {
+                labels: [
+                    "January",
+                    "February",
+                    "March",
+                    "April",
+                    "May",
+                    "June",
+                    "July",
+                    "August",
+                    "September",
+                    "October",
+                    "November",
+                    "December",
+                ],
+                datasets: [
+                    { data: [40, 20, 12, 60, 23, 54, 32, 60, 12, 32, 44, 55] },
+                ],
+            },
+            chartOptions: {
+                responsive: true,
+            },
+        };
+    },
+};
 </script>

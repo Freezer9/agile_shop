1021e6c68dfc25c40717cc50cc261cd0679994d3
arzaaqi <arzaaqi555@gmail.com>
2024-05-20 10:17:32 +0700

    admin

--- resources/js/Pages/Admin/Dashboard.vue
+++ resources/js/Pages/Admin/Dashboard.vue
@@ -6,7 +6,8 @@
             >
                 <div
                     class="border-2 border-dashed border-gray-300 rounded-lg dark:border-gray-600 h-32 md:h-64"
-                ></div>
+                >
+            </div>
                 <div
                     class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-32 md:h-64"
                 ></div>
@@ -19,7 +20,7 @@
             </div>
             <div
                 class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-96 mb-4"
-            ></div>
+            >besar</div>
             <div class="grid grid-cols-2 gap-4 mb-4">
                 <div
                     class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72"
@@ -54,6 +55,7 @@
         </div>
 
         <!-- TODO: Admin dashboard (Jeki, Alif) -->
+        
     </AdminLayout>
 </template>
 
